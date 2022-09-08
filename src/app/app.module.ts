300761ed1589a5fdf29756da0301a2016448cf6f
Miropolsky <Miropolskysk@yandex.ru>
2022-09-09 01:08:38 +0500

    add first page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
+import { HeadComponent } from './head/head.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { InfoComponent } from './main/info/info.component';
+import { CatalogComponent } from './main/catalog/catalog.component';
+import { AboutComponent } from './main/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FooterComponent,
+    HeadComponent,
+    InfoComponent,
+    CatalogComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
